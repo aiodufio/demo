f91d6980143dc64b266ecba00524772892135f37
aiodufio <904306821@qq.com>
2020-05-13 12:09:17 +0800

    a

--- a.java
+++ a.java
@@ -12,6 +12,4 @@ public class MyFirstJavaApplet extends Applet
              g.drawString(s,25,25);  
         }
 }
-————————————————
-版权声明：本文为CSDN博主「陈晨chen」的原创文章，遵循CC 4.0 BY-SA版权协议，转载请附上原文出处链接及本声明。
-原文链接：https://blog.csdn.net/chengchencheng/article/details/96852863
+
